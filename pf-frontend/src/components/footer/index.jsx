6515453bca57df01d8e4a2fe616ce764d0172356
IgorSilva-S <igor.silva1@aluno.ifsp.edu.br>
2025-03-29 00:51:18 -0300

    Footer okay

--- pf-frontend/src/components/footer/index.jsx
+++ pf-frontend/src/components/footer/index.jsx
@@ -4,13 +4,23 @@ import './style.css'
 export default function Footer() {
     return (
         <footer>
-            <div className="bigArea">
-                <p className="semiTitle"></p>
+            <div className="bigArea" textStyle="subtitle">
+                <p textStyle="title" textWeight="semibold">Conheça mais sobre a pawfect!</p>
+                <br />
+                Pawfect é um pet shop apaixonado por animais, oferecendo produtos e serviços de alta qualidade para garantir o bem-estar e felicidade dos pets. Com atendimento personalizado e um cuidado especial, somos a escolha perfeita para mimar seu melhor amigo.
             </div>
-            <div style={{gridArea: "simple-1"}}>Redes Sociais</div>
-            <div style={{gridArea: "simple-2"}}>Formas de pagamento</div>
-            <div style={{gridArea: "simple-3"}}>Politicas</div>
-            <div style={{gridArea: "simple-4"}}>Atendimento</div>
+            <div style={{gridArea: "simple-1"}}>
+                <p textWeight="semibold" textStyle="subtitle">Redes Sociais</p>
+            </div>
+            <div style={{gridArea: "simple-2"}}>
+                <p textWeight="semibold" textStyle="subtitle">Formas de pagamento</p>
+            </div>
+            <div style={{gridArea: "simple-3"}}>
+                <p textWeight="semibold" textStyle="subtitle">Políticas</p>
+            </div>
+            <div style={{gridArea: "simple-4"}}>
+                <p textWeight="semibold" textStyle="subtitle">Atendimento</p> 
+            </div>
         </footer>
     )
 }
