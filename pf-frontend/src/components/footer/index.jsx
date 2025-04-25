a4656bd2f2bd4a6fe5a0bacd98333384bf3806a5
camilly-vl <camilly.l@aluno.ifsp.edu.br>
2025-04-25 14:02:22 -0300

    footer-p1

--- pf-frontend/src/components/footer/index.jsx
+++ pf-frontend/src/components/footer/index.jsx
@@ -5,22 +5,29 @@ export default function Footer() {
     return (
         <footer>
             <div className="bigArea" textStyle="subtitle">
-                <p textStyle="title" textWeight="semibold">Conheça mais sobre a pawfect!</p>
+                <p textStyle="title" id="title-footer">Conheça mais sobre a pawfect!</p>
                 <br />
                 Pawfect é um pet shop apaixonado por animais, oferecendo produtos e serviços de alta qualidade para garantir o bem-estar e felicidade dos pets. Com atendimento personalizado e um cuidado especial, somos a escolha perfeita para mimar seu melhor amigo.
             </div>
-            <div style={{gridArea: "simple-1"}}>
-                <p textWeight="semibold" textStyle="subtitle">Redes Sociais</p>
+            <div className="other-area1" style={{gridArea: "simple-1"}}>
+                <p textStyle="subtitle">Redes Sociais</p>
+                <div className="sociais">
+                    <img className="img-footer" src="../../../public/img/footer/insta.png" alt="Instagram" />
+                    <img className="img-footer" src="../../../public/img/footer/facebook.png" alt="facebook" />
+                    <img className="img-footer" src="../../../public/img/footer/x.png" alt="X" />
+                    <img className="img-footer" src="../../../public/img/footer/youtube.png" alt="Youtube" />
+                </div>
             </div>
-            <div style={{gridArea: "simple-2"}}>
-                <p textWeight="semibold" textStyle="subtitle">Formas de pagamento</p>
+            <div className="other-area1"  style={{gridArea: "simple-2"}}>
+                <p textStyle="subtitle">Formas de pagamento</p>
+                <img src="../../../public/img/footer/payments.png" alt="Pagamento" />
             </div>
-            <div style={{gridArea: "simple-3"}}>
-                <p textWeight="semibold" textStyle="subtitle">Políticas</p>
+            <div className="other-area2" style={{gridArea: "simple-3"}}>
+                <p textStyle="subtitle">Políticas</p>
             </div>
-            <div style={{gridArea: "simple-4"}}>
-                <p textWeight="semibold" textStyle="subtitle">Atendimento</p> 
+            <div className="other-area2" style={{gridArea: "simple-4"}}>
+                <p textStyle="subtitle">Atendimento</p> 
             </div>
         </footer>
     )
-}
+}  
