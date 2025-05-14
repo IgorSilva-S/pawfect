d310311e1cfe695e58c0a36b006aa1aa3d1720bb
CAMILLY VITORIA LAZZARO FERREIRA <CG3026311@car.ifsp>
2025-05-14 12:12:44 -0300

    finish botoes-produtos

--- pf-frontend/src/components/home/botoes-produtos/index.jsx
+++ pf-frontend/src/components/home/botoes-produtos/index.jsx
@@ -5,22 +5,28 @@ export default function Home() {
     return (
         <div>
             <div id="botoes-produtos">
-                <button className="botao-produto"><img src="" alt="" /></button> 
-                <p className="texto-bp">Produtos para</p>
-
-                <button className="botao-produto"><img src="" alt="" /></button> 
-                <p className="texto-bp">Produtos para</p>
-
-                <button className="botao-produto"><img src="" alt="" /></button> 
-                <p className="texto-bp">Produtos para</p>
-
-                <button className="botao-produto"><img src="" alt="" /></button> 
-                <p className="texto-bp">Produtos para</p>
-
-                <button className="botao-produto"><img src="" alt="" /></button> 
-                <p className="texto-bp">Produtos para</p>
+                <div>
+                    <button className="botao-produto"><img src="../../../../public/img/animais-produtos/produtos-gatos.png" alt="Produtos para Gatos" /></button> 
+                    <p className="texto-bp">Produtos para Gatos</p>
+                </div>
+                <div>
+                    <button className="botao-produto"><img src="../../../../public/img/animais-produtos/produtos-caes.png" alt="Produtos para Cães" /></button> 
+                    <p className="texto-bp">Produtos para Cães</p>
+                </div>
+                <div>
+                    <button className="botao-produto"><img src="../../../../public/img/animais-produtos/produtos-passaros.png" alt="Produtos para Passáros" /></button> 
+                    <p className="texto-bp">Produtos para Pássaros</p>
+                </div>
+                <div>
+                    <button className="botao-produto"><img src="../../../../public/img/animais-produtos/produtos-roedores.png" alt="Produtos para Roedores" /></button> 
+                    <p className="texto-bp">Produtos para Roedores</p>
+                </div>
+                <div>
+                    <button className="botao-produto"><img src="../../../../public/img/animais-produtos/produtos-peixes.png" alt="Produtos para Peixes" /></button> 
+                    <p className="texto-bp">Produtos para Peixes</p>
+                </div>
             </div>
         </div>
-        
-    )
+         
+    ) 
 }    
--- pf-frontend/src/components/home/index.jsx
+++ pf-frontend/src/components/home/index.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import "./style.css";
 import Botoes from "../home/botoes";
+import BotoesProdutos from "../home/botoes-produtos";
 
 export default function Home() {
     return (
-            <Botoes />
+            <>
+                <Botoes />
+                <BotoesProdutos />
+            </>
     )
 }      
