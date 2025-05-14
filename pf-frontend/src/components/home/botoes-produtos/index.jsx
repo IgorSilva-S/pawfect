ddbc1e341d44f6dead8b9f276ee34e368299a366
camilly-vl <camilly.l@aluno.ifsp.edu.br>
2025-05-14 18:35:23 -0300

    finish plano-fidelidade

--- pf-frontend/src/components/home/botoes-produtos/index.jsx
+++ pf-frontend/src/components/home/botoes-produtos/index.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import "./style.css";
 
-export default function Home() {
+export default function BotoesProdutos() {
     return (
         <div>
             <div id="botoes-produtos">
--- pf-frontend/src/components/home/index.jsx
+++ pf-frontend/src/components/home/index.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import "./style.css";
 import Botoes from "../home/botoes";
 import BotoesProdutos from "../home/botoes-produtos";
+import Fidelidade from "../home/plano-fidelidade";
 
 export default function Home() {
     return (
             <>
                 <Botoes />
                 <BotoesProdutos />
+                <Fidelidade />
             </>
     )
 }      
