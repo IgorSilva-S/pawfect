86b7efd47742a75c5e0995e432e40a760f17f853
camilly-vl <camilly.l@aluno.ifsp.edu.br>
2025-05-15 10:35:35 -0300

    start-cart

--- pf-frontend/src/components/cart/index.jsx
+++ pf-frontend/src/components/cart/index.jsx
@@ -2,8 +2,8 @@ import React from "react";
 
 export default function Cart() {
     return (
-        <section>
-            <h1>Cart</h1>
-        </section>
-    )
+        <div className="cart">
+            
+        </div>
+    );
 }
