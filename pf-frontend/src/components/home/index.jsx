723f194c9c3a6349023ec6dfcd601292a1fb8b89
IgorSilva-S <igor.silva1@aluno.ifsp.edu.br>
2025-05-16 11:56:09 -0300

    Linked carousel and make new transitions

--- pf-frontend/src/components/home/index.jsx
+++ pf-frontend/src/components/home/index.jsx
@@ -4,10 +4,12 @@ import Botoes from "../home/botoes";
 import BotoesProdutos from "../home/botoes-produtos";
 import Fidelidade from "../home/plano-fidelidade";
 import Blog from "../home/blog";
+import Carousel from "./carousel";
 
 export default function Home() {
     return (
             <>
+                <Carousel />
                 <Botoes />
                 <BotoesProdutos />
                 <Fidelidade />
