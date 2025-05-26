bab9a917c8dcefb255c89d81da6988ae1875a5be
camilly-vl <camilly.l@aluno.ifsp.edu.br>
2025-05-26 20:26:18 -0300

    inicio produtos/recomendacoes

--- pf-frontend/src/components/cart/recomendacoes/index.jsx
+++ pf-frontend/src/components/cart/recomendacoes/index.jsx
@@ -9,20 +9,83 @@ export default function Recomendacoes() {
                 <div id="container-recomendacoes">
 
                 <div className="recomendacao">
-                    <img className="recomendacao-img" src="../../../../public/img/cart/" alt="Recomendação 1"></img>
-                    <div className="nota">
+                    {/* Imagens, textos figurativos (trocar pelos dados do banco) */}
+                    <img className="recomendacao-img" src="../../../../public/img/cart/recomendacoes-img/medsnack.png" alt="Recomendação 1"></img>
+                    <div className="nota1">
                         <p className="nota-p">4,9</p>
                         <img src="../../../../public/img/cart/star.png" alt="Estrela" />
                     </div>
-                    <p className="recomendacao-p"></p>
-                    <p className="recomendacao-preco"></p>
-                    <button className="recomendacao-b"><img src="../../../../public/img/menu/carrinho.png" alt="Botão carrinho" /></button> 
-                    <button className="recomendacao-b"><img src="../../../../public/img/menu/favs.png" alt="Botão Favoritos" /></button> 
+                    <p className="recomendacao-p">1Recomen dação 1Reco mendação 1Recomen 1</p>
+                    <p className="recomendacao-preco">$$,$$</p>
+                    <div className="recomendacao-bs">
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/carrinho.png" alt="Botão carrinho" /></button> 
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/favs.png" alt="Botão Favoritos" /></button> 
+                    </div>
+                </div>
+
+                 <div className="recomendacao">
+                    {/* Imagens, textos figurativos (trocar pelos dados do banco) */}
+                    <img className="recomendacao-img" src="../../../../public/img/cart/recomendacoes-img/medsnack.png" alt="Recomendação 1"></img>
+                    <div className="nota2">
+                        <p className="nota-p">4,9</p>
+                        <img src="../../../../public/img/cart/star.png" alt="Estrela" />
+                    </div>
+                    <p className="recomendacao-p">1Recomen dação 1Reco mendação 1Recomen 1</p>
+                    <p className="recomendacao-preco">$$,$$</p>
+                    <div className="recomendacao-bs">
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/carrinho.png" alt="Botão carrinho" /></button> 
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/favs.png" alt="Botão Favoritos" /></button> 
+                    </div>
+                </div>
+
+                 <div className="recomendacao">
+                    {/* Imagens, textos figurativos (trocar pelos dados do banco) */}
+                    <img className="recomendacao-img" src="../../../../public/img/cart/recomendacoes-img/medsnack.png" alt="Recomendação 1"></img>
+                    <div className="nota3">
+                        <p className="nota-p">4,9</p>
+                        <img src="../../../../public/img/cart/star.png" alt="Estrela" />
+                    </div>
+                    <p className="recomendacao-p">1Recomen dação 1Reco mendação 1Recomen 1</p>
+                    <p className="recomendacao-preco">$$,$$</p>
+                    <div className="recomendacao-bs">
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/carrinho.png" alt="Botão carrinho" /></button> 
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/favs.png" alt="Botão Favoritos" /></button> 
+                    </div>
+                </div>
+
+                 <div className="recomendacao">
+                    {/* Imagens, textos figurativos (trocar pelos dados do banco) */}
+                    <img className="recomendacao-img" src="../../../../public/img/cart/recomendacoes-img/medsnack.png" alt="Recomendação 1"></img>
+                    <div className="nota4">
+                        <p className="nota-p">4,9</p>
+                        <img src="../../../../public/img/cart/star.png" alt="Estrela" />
+                    </div>
+                    <p className="recomendacao-p">1Recomen dação 1Reco mendação 1Recomen 1</p>
+                    <p className="recomendacao-preco">$$,$$</p>
+                    <div className="recomendacao-bs">
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/carrinho.png" alt="Botão carrinho" /></button> 
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/favs.png" alt="Botão Favoritos" /></button> 
+                    </div>
+                </div>
+
+                 <div className="recomendacao">
+                    {/* Imagens, textos figurativos (trocar pelos dados do banco) */}
+                    <img className="recomendacao-img" src="../../../../public/img/cart/recomendacoes-img/medsnack.png" alt="Recomendação 1"></img>
+                    <div className="nota5">
+                        <p className="nota-p">4,9</p>
+                        <img src="../../../../public/img/cart/star.png" alt="Estrela" />
+                    </div>
+                    <p className="recomendacao-p">1Recomen dação 1Reco mendação 1Recomen 1</p>
+                    <p className="recomendacao-preco">$$,$$</p>
+                    <div className="recomendacao-bs">
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/carrinho.png" alt="Botão carrinho" /></button> 
+                        <button className="recomendacao-b"><img src="../../../../public/img/menu/favs.png" alt="Botão Favoritos" /></button> 
+                    </div>
                 </div>
 
                 </div>
                 
-        </div>
+        </div> 
     );
 }
   
